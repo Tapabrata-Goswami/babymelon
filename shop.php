339d1f6558b82e62e735946258fc1523344d7473
Sovan Majumder <majumdersovan4320@gmail.com>
2023-01-23 02:02:20 +0530

    add next button on shop page by sovan

--- shop.php
+++ shop.php
@@ -16,54 +16,6 @@ include 'nav.php';
 
 
 
-<!-- 
-<!DOCTYPE html>
-<html lang="en"> -->
-
-<!-- <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="shop.css">
-    <link rel="stylesheet" href="shop.js">
-    <link rel="stylesheet" href="shop.css.map">
-    <title>Shop</title>
-</head> -->
-
-<!-- <body> -->
-<!-- <div class="box">
-
-        <h3>Price <span>Range</span></h3>
-        <div class="values">
-            <div>$<span id="first">500</span></div> - <div>$<span id="second">2000</span></div>
-        </div>
-        <small>
-            Current Range:
-            <div>$<span id="third">2500</span></div>
-        </small>
-    
-        <div class="slider" data-value-0="#first" data-value-1="#second" data-range="#third"></div>
-    
-    </div>
-    
-    
-    <a class="dribbble" href="https://dribbble.com/shots/7268454-Rubber-Slider" target="_blank"><img src="https://cdn.dribbble.com/assets/dribbble-ball-mark-2bd45f09c2fb58dbbfb44766d5d1d07c5a12972d602ef8b32204d28fa3dda554.svg" alt=""></a>
-    <script src="https://cpwebassets.codepen.io/assets/common/stopExecutionOnTimeout-2c7831bb44f98c1391d6a4ffda0e1fd302503391ca806e7fcc7b9b87197aec26.js"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
-
-    <script src="shop.js"></script> -->
-
-<!-- <div class="filter-process">
-        
-        <h3>Filter by Price</h3>
-        
-        <div class="slidecontainer">
-          <input type="range" min="350" max="850" value="50" class="slider" id="myRange">
-          <p class="filter-price">&#8377<span id="demo"></span></p>
-        </div> -->
 
 <!-- Filter by catagories start -->
 
@@ -177,11 +129,16 @@ include 'nav.php';
 
                 ?>
 
-                <?php
-
-                include('next.php');
-
-                ?>
+<div class="nextpage-navigation" style="margin-bottom: 50px;">
+    <ul class="pagination">
+      <li class="pageNumber active"><a href="" class="nextpage-anchor">1</a></li>
+      <li class="pageNumber"><a href="" class="nextpage-anchor">2</a></li>
+      <li class="pageNumber"><a href="" class="nextpage-anchor">3</a></li>
+      <!-- <li class="pageNumber"><a href="">4</a></li>
+        <li class="pageNumber"><a href="">5</a></li>
+        <li class="pageNumber"><a href="">6</a></li> -->
+    </ul>
+  </div>
 
             </div>
 
@@ -190,20 +147,6 @@ include 'nav.php';
     <!-- Products container END -->
 </div>
 
-<!-- <script>
-        var slider = document.getElementById("myRange");
-        var output = document.getElementById("demo");
-        output.innerHTML = slider.value;
-
-        slider.oninput = function () {
-            output.innerHTML = this.value;
-        }
-    </script> -->
-
-<!-- 
-</body>
-
-</html> -->
 
 <?php
 
