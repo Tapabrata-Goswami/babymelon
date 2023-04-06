52eeb600ba84c1bb81e45aff41bbaf7a7a6ab851
Tapabrata Goswami <tapabratagoswami21@gmail.com>
2023-04-07 02:14:49 +0530

    blog update

--- single-blog.php
+++ single-blog.php
@@ -10,23 +10,38 @@
 <!-- Blog page html Start -->
 
 <section class="single-blog-page">
-    <div class="blog-content">
-        <h1 class="blog-heading">Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
-        <div class="blog-image">
-            <img class="blog-imges-main" src="./img/simon-berger-aZjw7xI3QAA-unsplash.jpg" alt="" width="90%">
-        </div>
-        <div class="blog-paragraph">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptas perferendis eius. A perspiciatis doloribus nesciunt optio reprehenderit temporibus, sit quisquam veniam minus maiores deleniti quis, ab ut laborum animi facilis praesentium totam, vel soluta velit libero odit provident! Esse magnam voluptatum repudiandae explicabo veniam nam deleniti culpa accusamus perferendis, ad sit dicta quasi, iste inventore dignissimos nobis excepturi? Quae aspernatur rem tenetur maxime recusandae molestiae nisi modi reiciendis blanditiis non vero maiores, eius nesciunt, voluptate dignissimos! Eaque asperiores nesciunt, dolores aperiam aut voluptate voluptates facilis ipsa, quia illo minus delectus? Distinctio, aliquam voluptates. Pariatur velit ab nisi impedit consequuntur. Exercitationem doloribus laudantium neque assumenda deleniti tempora cumque quasi corporis rem sit, dolorem maxime aperiam voluptate, placeat ducimus laborum dolore repellat consequuntur sequi, perspiciatis sunt ipsam? Unde, maiores expedita. Necessitatibus aliquam laudantium quaerat, adipisci beatae ipsam blanditiis maiores ducimus, doloribus error rerum! Illo optio, voluptatibus, explicabo totam quia labore quos perspiciatis cupiditate officiis dolorum ipsa natus voluptates nam culpa maxime recusandae. Ex voluptas accusamus sequi a repudiandae adipisci ab rerum nesciunt reiciendis incidunt! <br> Beatae unde a praesentium molestias modi expedita! Iure vel vero beatae sequi quo ullam ex tempore labore error quis! Commodi quis voluptates cumque. Eos, cupiditate vitae! Ex inventore excepturi, asperiores aperiam nostrum illum necessitatibus libero numquam impedit deleniti sed officiis assumenda tempore. Necessitatibus sed velit tenetur sit cumque repudiandae! A iure natus, ea pariatur corrupti asperiores minima molestias, dolore accusantium illum ipsa exercitationem! Asperiores exercitationem delectus sed sit sapiente, magni deserunt placeat impedit esse nesciunt eligendi earum eum pariatur cum. Aliquam modi doloremque commodi harum culpa eveniet, sapiente inventore? Error beatae vitae harum sit ipsam iste enim aut laudantium hic, pariatur est eaque consequuntur architecto.</p>
-            <!-- Suggested product -->
-            <div class="suggest-product">
-                <div class="img1">
-                    <img src="https://cdn2.hubspot.net/hub/215841/file-3945428210-jpg/blog-files/new-product-blog-602x347pix.jpg" alt="product" width="400" height="400">
-                </div>
-                <div class="blog-paragraph">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore earum aliquam impedit a. Porro dolores excepturi minima. Magnam repellendus doloremque natus iusto, earum sed sit. Id non dolore suscipit sed saepe totam consectetur error perferendis fuga ducimus voluptatem commodi reprehenderit delectus temporibus, sint numquam explicabo doloribus asperiores assumenda odio. Eum nam hic rem nesciunt atque et a! Nesciunt ipsa adipisci magni ipsam nihil earum nam illum maiores, quas dignissimos consectetur blanditiis officiis corrupti quae at repellendus omnis, eligendi quis facere velit ex enim esse voluptatem deserunt! Dolorum eius expedita aut dignissimos possimus alias dolore beatae consequatur voluptas aspernatur? Necessitatibus itaque consequuntur culpa perspiciatis eaque dolor eveniet asperiores vitae quisquam rem. Omnis molestias minima, asperiores rerum cum, hic repellendus at culpa voluptatibus quaerat, voluptates non cupiditate nobis impedit dicta repellat aliquam in soluta commodi deserunt quidem quisquam! Impedit suscipit voluptatum quam nobis quas? Praesentium, inventore libero, maxime id odio nam accusantium magni blanditiis sint, in eaque aut. Possimus recusandae, illo, nemo odit, voluptatum laborum et accusamus quos voluptatibus quis nam quidem alias! Ut provident eum fugit cumque amet, voluptates, enim ipsa ab incidunt omnis, ad animi quibusdam nulla. Dolorem, doloremque modi. Fuga dolore debitis sequi nostrum reiciendis minima facilis tempora dicta!
-                </p>
+    <div class="single-blog-page-all-content">
+        <div class="blog-content">
+            <h1 class="blog-heading">Lorem ipsum dolor sit amet consect adipisicing.</h1>
+            <div class="blog-image">
+                <img class="blog-imges-main" src="https://mamaearth.in/blog/wp-content/uploads/2023/04/unnamed-4-1-1.webp" alt="" width="90%">
             </div>
-            </div>  
+            <div class="author-share">
+                <div class="author">by <span>bm</span></div>
+            <div class="share">SHARE <i class="fa-brands    fa-facebook"></i> <i class="fa-brands fa-instagram"></i> <i class="fa-brands fa-whatsapp"></i>
+                </div>
+            </div>
+            <div class="blog-paragraph">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptas perferendis eius. A perspiciatis doloribus nesciunt optio reprehenderit temporibus, sit quisquam veniam minus maiores deleniti quis, ab ut laborum animi facilis praesentium totam, vel soluta velit libero odit provident! Esse magnam voluptatum repudiandae explicabo veniam nam deleniti culpa accusamus perferendis, ad sit dicta quasi, iste inventore dignissimos nobis excepturi? Quae aspernatur rem tenetur maxime recusandae molestiae nisi modi reiciendis blanditiis non vero maiores, eius nesciunt, voluptate dignissimos! Eaque asperiores nesciunt, dolores aperiam aut voluptate voluptates facilis ipsa, quia illo minus delectus? Distinctio, aliquam voluptates. Pariatur velit ab nisi impedit consequuntur. Exercitationem doloribus laudantium neque assumenda deleniti tempora cumque quasi corporis rem sit, dolorem maxime aperiam voluptate, placeat ducimus laborum dolore repellat consequuntur sequi, perspiciatis sunt ipsam? Unde, maiores expedita. Necessitatibus aliquam laudantium quaerat, adipisci beatae ipsam blanditiis maiores ducimus, doloribus error rerum! Illo optio, voluptatibus, explicabo totam quia labore quos perspiciatis cupiditate officiis dolorum ipsa natus voluptates nam culpa maxime recusandae. Ex voluptas accusamus sequi a repudiandae adipisci ab rerum nesciunt reiciendis incidunt! <br> Beatae unde a praesentium molestias modi expedita! Iure vel vero beatae sequi quo ullam ex tempore labore error quis! Commodi quis voluptates cumque. Eos, cupiditate vitae! Ex inventore excepturi, asperiores aperiam nostrum illum </p>
+            </div> 
+            <!-- Suggested product start -->
+            <div class="suggest-product">
+                <div class="title-product">Trending Product</div>
+                <div class="img1">
+                    <a href="#">
+                        <img src="https://babymelon.in/wp-content/uploads/2023/01/5.png" alt="product" width="300" height="300" style="margin-bottom:15px;">
+                    </a>
+                </div>
+                <a href="#" class="button-blog-product"> Buy Now</a>
+            </div>
+            <!-- Suggested product End -->
+            <div class="blog-paragraph">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptas perferendis eius. A perspiciatis doloribus nesciunt optio reprehenderit temporibus, sit quisquam veniam minus maiores deleniti quis, ab ut laborum animi facilis praesentium totam, vel soluta dicta quasi, iste inventore dignissimos nobis excepturi? Quae aspernatur rem tenetur maxime recusandae molestiae nisi modi reiciendis </p>
+            </div> 
+        </div>
+
+        <!-- side bar -->
+        <div class="top-blogpost">
 
         </div>
     </div>
